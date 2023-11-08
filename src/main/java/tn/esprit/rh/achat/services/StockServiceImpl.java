a8f16f0b9efc2e6de0e3d6868a9cf509dee94104
cloud / Devops engineer <98840383+ZayaniHassen@users.noreply.github.com>
2023-11-08 04:10:15 +0100

    Update StockServiceImpl.java

--- src/main/java/tn/esprit/rh/achat/services/StockServiceImpl.java
+++ src/main/java/tn/esprit/rh/achat/services/StockServiceImpl.java
@@ -1,34 +1,87 @@
-package tn.esprit.devops_project.services;
-
-import lombok.AllArgsConstructor;
+package tn.esprit.rh.achat.services;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import tn.esprit.devops_project.services.Iservices.IStockService;
-import tn.esprit.devops_project.entities.Stock;
-import tn.esprit.devops_project.repositories.StockRepository;
+import tn.esprit.rh.achat.entities.Stock;
+import tn.esprit.rh.achat.repositories.StockRepository;
+import tn.esprit.rh.achat.services.IStockService;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @Service
-@AllArgsConstructor
+@Slf4j
 public class StockServiceImpl implements IStockService {
 
-   private final StockRepository stockRepository;
-
-    @Override
-    public Stock addStock(Stock stock) {
-        return stockRepository.save(stock);
-    }
-
-    @Override
-    public Stock retrieveStock(Long id) {
-        return stockRepository.findById(id).orElseThrow(() -> new NullPointerException("Stock not found"));
-    }
-
-    @Override
-    public List<Stock> retrieveAllStock() {
-        return stockRepository.findAll();
-    }
+	@Autowired
+	StockRepository stockRepository;
 
 
+	@Override
+	public List<Stock> retrieveAllStocks() {
+		// récuperer la date à l'instant t1
+		log.info("In method retrieveAllStocks");
+		List<Stock> stocks = (List<Stock>) stockRepository.findAll();
+		for (Stock stock : stocks) {
+			log.info(" Stock : " + stock);
+		}
+		log.info("out of method retrieveAllStocks");
+		// récuperer la date à l'instant t2
+		// temps execution = t2 - t1
+		return stocks;
+	}
+
+	@Override
+	public Stock addStock(Stock s) {
+		// récuperer la date à l'instant t1
+		log.info("In method addStock");
+		return stockRepository.save(s);
+
+	}
+
+	@Override
+	public void deleteStock(Long stockId) {
+		log.info("In method deleteStock");
+		stockRepository.deleteById(stockId);
+
+	}
+
+	@Override
+	public Stock updateStock(Stock s) {
+		log.info("In method updateStock");
+		return stockRepository.save(s);
+	}
+
+	@Override
+	public Stock retrieveStock(Long stockId) {
+		long start = System.currentTimeMillis();
+		log.info("In method retrieveStock");
+		Stock stock = stockRepository.findById(stockId).orElse(null);
+		log.info("out of method retrieveStock");
+		long elapsedTime = System.currentTimeMillis() - start;
+		log.info("Method execution time: " + elapsedTime + " milliseconds.");
+
+		return stock;
+	}
+
+	@Override
+	public String retrieveStatusStock() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+		Date now = new Date();
+		String msgDate = sdf.format(now);
+		String finalMessage = "";
+		String newLine = System.getProperty("line.separator");
+		List<Stock> stocksEnRouge = (List<Stock>) stockRepository.retrieveStatusStock();
+		for (int i = 0; i < stocksEnRouge.size(); i++) {
+			finalMessage = newLine + finalMessage + msgDate + newLine + ": le stock "
+					+ stocksEnRouge.get(i).getLibelleStock() + " a une quantité de " + stocksEnRouge.get(i).getQte()
+					+ " inférieur à la quantité minimale a ne pas dépasser de " + stocksEnRouge.get(i).getQteMin()
+					+ newLine;
+
+		}
+		log.info(finalMessage);
+		return finalMessage;
+	}
 
 }
