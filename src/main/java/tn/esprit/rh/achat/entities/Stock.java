dd476f0eac2ca49e3e6d58bae6409b02f4b2ef7d
cloud / Devops engineer <98840383+ZayaniHassen@users.noreply.github.com>
2023-11-08 04:22:29 +0100

    Update Stock.java

--- src/main/java/tn/esprit/rh/achat/entities/Stock.java
+++ src/main/java/tn/esprit/rh/achat/entities/Stock.java
@@ -1,13 +1,9 @@
 package tn.esprit.rh.achat.entities;
-
 import java.io.Serializable;
-import java.util.Set;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -29,7 +25,6 @@ public class Stock implements Serializable {
 	private Integer qteMin;
 //	@OneToMany(mappedBy = "stock")
 //	@JsonIgnore
-//	private Set<Produit> produits;
 	public Stock(String libelleStock, Integer qte, Integer qteMin) {
 		super();
 		this.libelleStock = libelleStock;
