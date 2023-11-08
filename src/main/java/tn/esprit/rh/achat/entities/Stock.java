58c7f7c71e331c41da5acc4aa8eb25bb96263ca8
cloud / Devops engineer <98840383+ZayaniHassen@users.noreply.github.com>
2023-11-08 03:10:29 +0100

    Update Stock.java

--- src/main/java/tn/esprit/rh/achat/entities/Stock.java
+++ src/main/java/tn/esprit/rh/achat/entities/Stock.java
@@ -1,40 +1,23 @@
-package tn.esprit.rh.achat.entities;
+package tn.esprit.devops_project.entities;
 
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Set;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Stock implements Serializable {
-	private static final long serialVersionUID = 1L;
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long idStock;
-	private String libelleStock;
-	private Integer qte;
-	private Integer qteMin;
-//	@OneToMany(mappedBy = "stock")
-//	@JsonIgnore
-//	private Set<Produit> produits;
-	public Stock(String libelleStock, Integer qte, Integer qteMin) {
-		super();
-		this.libelleStock = libelleStock;
-		this.qte = qte;
-		this.qteMin = qteMin;
-	}
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long idStock;
+    String title;
+    @OneToMany(mappedBy = "stock")
+    Set<Product> products;
 }
