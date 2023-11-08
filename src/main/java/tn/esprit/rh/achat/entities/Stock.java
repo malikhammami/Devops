a3707cc59b939a08c175e68913cf0640397838a8
cloud / Devops engineer <98840383+ZayaniHassen@users.noreply.github.com>
2023-11-08 04:04:52 +0100

    Update Stock.java

--- src/main/java/tn/esprit/rh/achat/entities/Stock.java
+++ src/main/java/tn/esprit/rh/achat/entities/Stock.java
@@ -1,40 +1,34 @@
 package tn.esprit.rh.achat.entities;
-import java.io.Serializable;
-import java.util.Set;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Entity
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-public class Stock implements Serializable {
-	private static final long serialVersionUID = 1L;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long idStock;
-	private String libelleStock;
-	private Integer qte;
-	private Integer qteMin;
-	//	@OneToMany(mappedBy = "stock")
-//	@JsonIgnore
-//	private Set<Produit> produits;
-	public Stock(String libelleStock, Integer qte, Integer qteMin) {
-		super();
-		this.libelleStock = libelleStock;
-		this.qte = qte;
-		this.qteMin = qteMin;
-	}
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+public class StockTest {
+    private Stock stock;
 
+    @BeforeEach
+    public void setUp() {
+        stock = Stock.builder()
+            .libelleStock("Test Stock")
+            .qte(100)
+            .qteMin(10)
+            .build();
+    }
+
+    @Test
+    public void testGetLibelleStock() {
+        assertEquals("Test Stock", stock.getLibelleStock());
+    }
+
+    @Test
+    public void testGetQte() {
+        assertEquals(100, stock.getQte());
+    }
+
+    @Test
+    public void testGetQteMin() {
+        assertEquals(10, stock.getQteMin());
+    }
 }
