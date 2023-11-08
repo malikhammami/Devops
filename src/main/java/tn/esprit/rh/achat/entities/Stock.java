bb24a3a205db53f30258a986e6320cdbc3bcd267
cloud / Devops engineer <98840383+ZayaniHassen@users.noreply.github.com>
2023-11-08 02:36:15 +0100

    Update Stock.java

--- src/main/java/tn/esprit/rh/achat/entities/Stock.java
+++ src/main/java/tn/esprit/rh/achat/entities/Stock.java
@@ -1,10 +1,13 @@
 package tn.esprit.rh.achat.entities;
 
 import java.io.Serializable;
+import java.util.Set;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,50 +19,23 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Stock implements Serializable {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long idStock;
-    private String libelleStock;
-    private Integer qte;
-    private Integer qteMin;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idStock;
+	private String libelleStock;
+	private Integer qte;
+	private Integer qteMin;
+//	@OneToMany(mappedBy = "stock")
+//	@JsonIgnore
+//	private Set<Produit> produits;
+	public Stock(String libelleStock, Integer qte, Integer qteMin) {
+		super();
+		this.libelleStock = libelleStock;
+		this.qte = qte;
+		this.qteMin = qteMin;
+	}
 
-    public Stock(String libelleStock, Integer qte, Integer qteMin) {
-        this.libelleStock = libelleStock;
-        this.qte = qte;
-        this.qteMin = qteMin;
-    }
 
-    public static StockBuilder builder() {
-        return new StockBuilder();
-    }
-
-    public static class StockBuilder {
-        private String libelleStock;
-        private Integer qte;
-        private Integer qteMin;
-
-        private StockBuilder() {
-        }
-
-        public StockBuilder libelleStock(String libelleStock) {
-            this.libelleStock = libelleStock;
-            return this;
-        }
-
-        public StockBuilder qte(Integer qte) {
-            this.qte = qte;
-            return this;
-        }
-
-        public StockBuilder qteMin(Integer qteMin) {
-            this.qteMin = qteMin;
-            return this;
-        }
-
-        public Stock build() {
-            return new Stock(libelleStock, qte, qteMin);
-        }
-    }
 }
